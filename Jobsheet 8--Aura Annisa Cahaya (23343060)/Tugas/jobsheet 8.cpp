2cb726101347106817293c2a564ddb25e32580be
AuraAnnisa <147116937+AuraAnnisa@users.noreply.github.com>
2023-12-04 12:23:50 +0700

    Add files via upload

---
+++ Jobsheet 8--Aura Annisa Cahaya (23343060)/Tugas/jobsheet 8.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int sum(int a, int b);
+
+int main (){
+	int number, result;
+	
+	printf("Masukkan dua nilai bilangan positif: ");
+	scanf("%d" "%d", &number, &number);
+	
+	if (a == || b==){
+		
+	};
+	
+}
