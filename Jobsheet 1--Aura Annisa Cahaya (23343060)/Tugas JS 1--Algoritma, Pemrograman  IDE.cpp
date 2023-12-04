496c02ac6828896ab5eb21d9d320f23bf59fa723
AuraAnnisa <147116937+AuraAnnisa@users.noreply.github.com>
2023-12-04 12:12:03 +0700

    Add files via upload

---
+++ Jobsheet 1--Aura Annisa Cahaya (23343060)/Tugas JS 1--Algoritma, Pemrograman  IDE.cpp
@@ -0,0 +1,12 @@
+// created by Aura Annisa Cahaya (23343060)
+
+#include <stdio.h> // header yang berisikan library
+
+int main(){ // tempat program dieksekusi
+	
+	// menampilkan output Hello World
+	printf("Hello world");
+	
+	// membalikkan nilai 0
+	return 0;
+}
